4a286f249616697f505b7939daafb0a266e3b293
imrushka <62286965+imrushka@users.noreply.github.com>
2021-03-01 18:45:47 +0600

    problem_1

--- main.py
+++ main.py
@@ -14,8 +14,12 @@ class DBSample(QMainWindow):
         self.connection = sqlite3.connect("coffee.db")
         # По умолчанию будем выводить все данные из таблицы films
         self.select_data()
+        self.w = None
+        self.show_new_window()
+
 
     def select_data(self):
+        print('qwr')
         # Получим результат запроса,
         # который ввели в текстовое поле
         res = self.connection.cursor().execute("SELECT * FROM COFFEE").fetchall()
@@ -29,6 +33,48 @@ class DBSample(QMainWindow):
             for j, elem in enumerate(row):
                 self.tableWidget.setItem(
                     i, j, QTableWidgetItem(str(elem)))
+
+    def closeEvent(self, event):
+        # При закрытии формы закроем и наше соединение
+        # с базой данных
+        self.connection.close()
+
+    def show_new_window(self):
+        if self.w is None:
+            self.w = AnotherWindow()
+        self.w.show()
+
+class AnotherWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.connection = sqlite3.connect("coffee.db")
+        self.pushButton.clicked.connect(self.select_data)
+        # По умолчанию будем выводить все данные из таблицы films
+        self.select_data()
+
+    def select_data(self):
+        # Получим результат запроса,
+        # который ввели в текстовое поле
+        query = self.textEdit.toPlainText()
+        try:
+            res = self.connection.cursor().execute(query).fetchall()
+            # Заполним размеры таблицы
+            self.tableWidget.setColumnCount(4)
+            self.tableWidget.setRowCount(0)
+            # Заполняем таблицу элементами
+            for i, row in enumerate(res):
+                self.tableWidget.setRowCount(
+                    self.tableWidget.rowCount() + 1)
+                for j, elem in enumerate(row):
+                    self.tableWidget.setItem(
+                        i, j, QTableWidgetItem(str(elem)))
+            ex.select_data()
+            self.label_2.setText("success")
+
+        except Exception as e:
+            print(e)
+            self.label_2.setText("error")
 
     def closeEvent(self, event):
         # При закрытии формы закроем и наше соединение
